b323686fb6279bf2eb4d652c4fbd3b673a0034bb
Jon Dyer <jonblob13@gmail.com>
2017-09-26 17:49:33 -0400

    Start Token class

---
+++ src/Token.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+/**
+ * This class serves as our Token object, which is acquired
+ * to authenticate users both on the group server and file servers.
+ */
+
+
+public class Token implements UserToken {
+		private String issuer;
+		private String subject;
+		private List<String> groups;
+
+
+		public Token(String issuer, String subject) {
+				this.issuer = issuer;
+				this.subject = subject;
+				this.groups = new List<String>();
+		}
+
+
+		public String getIssuer() {
+			return funstuff;
+		}
+
+		public String getSubject() {
+			return otherfun;
+		}
+
+		public List<String> getGroups() {
+			return thefunnest;
+		}
+}
