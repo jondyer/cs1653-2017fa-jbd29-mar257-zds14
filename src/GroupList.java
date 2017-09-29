708bfb947ac71c648319e01370feafcc087a9d25
Max Reno <mar257@pitt.edu>
2017-09-29 09:05:19 -0400

    Untabify some files & adjust some brackets

--- src/GroupList.java
+++ src/GroupList.java
@@ -11,53 +11,43 @@ public class GroupList {
         return list.keySet().toArray(new String[0]);
     }
 
-    public synchronized void addGroup(String groupName)
-    {
+    public synchronized void addGroup(String groupName) {
         Group newGroup = new Group();
         list.put(groupName, newGroup);
     }
 
-    public synchronized void deleteGroup(String groupName)
-    {
+    public synchronized void deleteGroup(String groupName) {
         list.remove(groupName);
     }
 
-    public synchronized boolean checkGroup(String groupName)
-    {
+    public synchronized boolean checkGroup(String groupName) {
         if(list.containsKey(groupName))
             return true;
         return false;
     }
 
     //TODO: Finish all below
-    public synchronized ArrayList<String> getUserGroups(String username)
-    {
+    public synchronized ArrayList<String> getUserGroups(String username) {
         return list.get(username).getGroups();
     }
 
-    public synchronized ArrayList<String> getUserOwnership(String username)
-    {
+    public synchronized ArrayList<String> getUserOwnership(String username) {
         return list.get(username).getOwnership();
     }
 
-    public synchronized void addGroup(String user, String groupname)
-    {
+    public synchronized void addGroup(String user, String groupname) {
         list.get(user).addGroup(groupname);
     }
 
-    public synchronized void removeGroup(String user, String groupname)
-    {
+    public synchronized void removeGroup(String user, String groupname) {
         list.get(user).removeGroup(groupname);
     }
 
-    public synchronized void addOwnership(String user, String groupname)
-    {
+    public synchronized void addOwnership(String user, String groupname) {
         list.get(user).addOwnership(groupname);
     }
 
-    public synchronized void removeOwnership(String user, String groupname)
-    {
+    public synchronized void removeOwnership(String user, String groupname) {
         list.get(user).removeOwnership(groupname);
     }
-
 }
--- src/Token.java
+++ src/Token.java
@@ -8,38 +8,38 @@ import java.util.ArrayList;
 
 
 public class Token implements UserToken, java.io.Serializable {
-		private String issuer;
-		private String subject;
-		private List<String> groups;
+    private String issuer;
+    private String subject;
+    private List<String> groups;
 
 
-		public Token(String issuer, String subject) {
-				this.issuer = issuer;
-				this.subject = subject;
-				this.groups = new ArrayList<String>();
-		}
+    public Token(String issuer, String subject) {
+        this.issuer = issuer;
+        this.subject = subject;
+        this.groups = new ArrayList<String>();
+    }
 
-		public Token(String issuer, String subject, ArrayList<String> groups) {
-				this.issuer = issuer;
-				this.subject = subject;
-				this.groups = groups;
-		}
+    public Token(String issuer, String subject, ArrayList<String> groups) {
+        this.issuer = issuer;
+        this.subject = subject;
+        this.groups = groups;
+    }
 
-		public String getIssuer() {
-			return issuer;
-		}
+    public String getIssuer() {
+      return issuer;
+    }
 
-		public String getSubject() {
-			return subject;
-		}
+    public String getSubject() {
+      return subject;
+    }
 
-		public List<String> getGroups() {
-			return groups;
-		}
+    public List<String> getGroups() {
+      return groups;
+    }
 
-		// TODO: Handle duplicate groups this is the most important -- use a hashmap!
-		public void addGroup(String group) {
-			this.groups.add(group);
-		}
+    // TODO: Handle duplicate groups this is the most important -- use a hashmap!
+    public void addGroup(String group) {
+      this.groups.add(group);
+    }
 
 }
