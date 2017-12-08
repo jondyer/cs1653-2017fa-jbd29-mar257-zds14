cd18fe777961fce8fe56c6fffafa16c2d6fa84a4
Jon Dyer <jonblob13@gmail.com>
2017-12-07 20:23:04 -0500

    are fun!

--- scripts/attack_T10.sh
+++ scripts/attack_T10.sh
@@ -4,14 +4,13 @@
 jar_name=bcprov-jdk15on-158.jar
 thePath="cs1653-2017fa-jbd29-mar257-zds14"
 myTerm="terminator"
+ct=30;
 
 clientCmd="java -cp .:$jar_name RunClientApp"
 
 if [[ $# > 1 ]]; then
   #statements
-  ct = $1;
-else
-  ct = 30;
+  ct=$1;
 fi
 
 
@@ -25,7 +24,7 @@ fi
 
 cp "../scripts/attInput" "../scripts/T10_input"
 echo "aaaaaaaa" >> "../scripts/T10_input"
-attIn="../scripts/T10input"
+attIn="../scripts/T10_input"
 
 
 for (( i = 0; i < ct ; i++ )); do
